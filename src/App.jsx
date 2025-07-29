0a294ffe2f513db0512c7823c8b8409176588900
GPR <2300031613@kluniversity.in>
2025-07-29 20:13:27 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
-
+import './style.css'
 function App() {
   return (
     <div className="container">
       <h1>Welcome to My React Project - S205</h1>
+      <p>CI/CD && Cloud DevOps</p>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
     </div>
   );
